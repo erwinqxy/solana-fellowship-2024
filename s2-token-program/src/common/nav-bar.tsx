748ae089bf966db1847e2a3da05add5512ba3da9
Erwin Quek <72030222+erwinqxy@users.noreply.github.com>
2024-08-05 16:01:58 +0800

    feat: add github

--- s2-token-program/src/common/nav-bar.tsx
+++ s2-token-program/src/common/nav-bar.tsx
@@ -5,16 +5,26 @@ import {
   WalletDisconnectButton,
   WalletMultiButton,
 } from "@solana/wallet-adapter-react-ui";
+import GitHubButton from 'react-github-btn';
+
+
 // import '@solana/wallet-adapter-react-ui/styles.css';
 
 export default function NavBar() {
   return (
-    <nav className="p-4 flex justify-around bg-zinc-800">
-      <div style={{ paddingRight: "10px" }}>
-        <WalletMultiButton />
+    <nav className='p-4 flex justify-around bg-zinc-800'>
+      <div  className='p-4 flex' style={{ flex: 1 }}>
+        <div style={{ paddingRight: '10px' }}>
+          <WalletMultiButton />
+        </div>
+        <div>
+          <WalletDisconnectButton />
+        </div>
       </div>
       <div>
-        <WalletDisconnectButton />
+        <GitHubButton href='https://github.com/erwinqxy/solana-fellowship-2024/tree/main/s2-token-program'>
+          s2-token-program repo by erwinqxy
+        </GitHubButton>
       </div>
     </nav>
   );
